500fb7deb6f2c6f5030dc66d8baede6a001df1b2
μT <37547640+mutichung@users.noreply.github.com>
2024-01-17 20:25:51 +0800

    Currency Rate Updates (#6)
    * Allow `get_rate()` to fallback to latest date.
    * Add basic logging.
    * Add `datetime` module support.

--- src/aag/utils.py
+++ src/aag/utils.py
@@ -1,3 +1,5 @@
+import datetime
+import logging
 import os
 import requests
 from itertools import product
@@ -8,9 +10,24 @@ CURRENCY_BASE_URLS = (
 )
 CURRENCY_SUFFIXES = (".min.json", ".json")
 
+logger = logging.Logger(__name__)
 
-def get_rate(src: str, dest: str, date: str = "latest") -> float:
+
+def get_rate(src: str, dest: str, date: datetime.date | str = "latest") -> float:
+    return _get_rate_impl(src, dest, date)
+
+
+def _get_rate_impl(
+    src: str,
+    dest: str,
+    date: datetime.date | str = "latest",
+    should_fallback: bool = True,
+) -> float:
     rate = None
+
+    if isinstance(date, datetime.date):
+        date = date.isoformat()
+
     for base_url, suffix in product(CURRENCY_BASE_URLS, CURRENCY_SUFFIXES):
         url = os.path.join(base_url, date, "currencies", src, dest + suffix)
         rate = requests.get(url)
@@ -18,6 +35,12 @@ def get_rate(src: str, dest: str, date: str = "latest") -> float:
             break
 
     if not rate:
+        if should_fallback:
+            logger.warning(
+                f"Falling back to latest ({datetime.date.today().isoformat()}) rate since the following combination "
+                f"fails all attempts: {[src, dest, date]}."
+            )
+            return _get_rate_impl(src, dest, date="latest", should_fallback=False)
         raise RuntimeError(f"Cannot get currency rate {src} -> {dest} on date {date}.")
 
     return rate.json()[dest]
